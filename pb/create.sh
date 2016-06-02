84d9645235d49461848c14493b6a65513ac309e0
zergl <e3.gemini@qq.com>
2016-06-02 13:19:15 +0800

    [refactor] refactor pb/create.sh
    Signed-off-by: zergl <e3.gemini@qq.com>

--- pb/create.sh
+++ pb/create.sh
@@ -1,15 +1,13 @@
-#!/bin/sh
+#!/bin/env bash
+
 SRC_DIR=./
 DST_DIR=./gen
 
-#C++
-mkdir -p $DST_DIR/cpp
-protoc -I=$SRC_DIR --cpp_out=$DST_DIR/cpp/ $SRC_DIR/*.proto
+lang_list="cpp java python"
+for lang in $lang_list
+do
+    echo " Creating proto source files for $lang"
+    mkdir -p "$DST_DIR/$lang"
+    protoc -I=$SRC_DIR --${lang}_out=$DST_DIR/$lang $SRC_DIR/*.proto
+done
 
-#JAVA
-mkdir -p $DST_DIR/java
-protoc -I=$SRC_DIR --java_out=$DST_DIR/java/ $SRC_DIR/*.proto
-
-#PYTHON
-mkdir -p $DST_DIR/python
-protoc -I=$SRC_DIR --python_out=$DST_DIR/python/ $SRC_DIR/*.proto
