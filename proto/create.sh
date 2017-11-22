03d580dd3d32289bb5ec93685aefe17871317c81
zergl <e3.gemini@qq.com>
2017-11-23 02:39:12 +0800

    [add]1.update libproto*.a/liblog4cxx.a 2.update proto generating script.

--- proto/create.sh
+++ proto/create.sh
@@ -2,6 +2,7 @@
 
 SRC_DIR=./
 DST_DIR=./gen
+PROTO_DIR=../server/src/base/pb/protocol
 
 lang_list="cpp java python"
 for lang in $lang_list
@@ -10,4 +11,8 @@ do
     mkdir -p "$DST_DIR/$lang"
     protoc -I=$SRC_DIR --${lang}_out=$DST_DIR/$lang $SRC_DIR/*.proto
 done
+
+rm -rf $PROTO_DIR
+mkdir -p $PROTO_DIR
+cp -r $DST_DIR/* $PROTO_DIR
 
--- proto/sync.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-CPP_DIR=../server/src/base/pb/protocol
-DST_DIR=./gen
-
-#C++
-cp $DST_DIR/cpp/* $CPP_DIR/
-
-rm -rf ./gen
